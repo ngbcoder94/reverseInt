80e5f32cc8eb14db2ba8d4803bd522bd63230e9c
Nick <burkettng@appstate.edu>
2023-12-05 22:01:28 -0500

    Got it but sloppy

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -13,14 +13,17 @@ class ReverseString {
     public: 
         int32_t reverseInt(int32_t  num){
 
-            int itr = 0;            //Need to create a counter variable. 
+            int itr = 1;            //Need to create a counter variable. 
             int32_t finalVal = 0;   //Variable for final integer.
 
             //While loop to iterate over the given number int by int
             while(num){
-                printf("%d", (num %10));    //Print out the current digit
-                num /= 10;                  //Adjust num accordingly
+                printf("%d", (num %10));    		//Print out the current digit
+				finalVal = finalVal * 10 + (num%10);		//Create the new number
+                num /= 10;  						//Adjust num accordingly
+				itr ++; 							//Increment itr by 1
             }
+			printf("%d\n", finalVal);
             return 0;                       //Will eventually return the final num
         }
 };
