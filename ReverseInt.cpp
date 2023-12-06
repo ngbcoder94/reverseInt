568be2668ff937fc7efa51ce0ad742201a42a9aa
Nick <burkettng@appstate.edu>
2023-12-05 21:32:49 -0500

    Able to iterate over int

--- ReverseInt.cpp
+++ ReverseInt.cpp
@@ -1,1 +1,33 @@
-//Hello!!! This is my attempt at the reverse int problem on Leetcode
+/*
+ * This is a program I am creating that will read in
+ * a string and return it in reverse order. 
+ *
+ * Problem is for a Leet Code submission - Reverse String
+*/
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+class ReverseString {
+    public: 
+        int32_t reverseInt(int32_t  num){
+
+            int itr = 0;            //Need to create a counter variable. 
+            int32_t finalVal = 0;   //Variable for final integer.
+
+            //While loop to iterate over the given number int by int
+            while(num){
+                printf("%d", (num %10));    //Print out the current digit
+                num /= 10;                  //Adjust num accordingly
+            }
+            return 0;                       //Will eventually return the final num
+        }
+};
+int main(){
+
+    ReverseString obj;
+
+    obj.reverseInt(1234);
+    return 0; 
+}
